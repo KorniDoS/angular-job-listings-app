5eaa993c121f6a48ba2a7fc34edad5caf199bb83
Mihai Mirzac <mihaimarzac@gmail.com>
2022-09-18 20:47:35 +0300

    Added mobile styling and main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { JobModel } from './models/job.model';
+import { OnInit } from '@angular/core';
+import { JobService } from './services/job.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'job-listings';
+export class AppComponent implements OnInit {
+
+
+  jobs: JobModel[] = [];
+
+  constructor(private jobService: JobService){}
+
+
+  ngOnInit(): void{
+    this.jobService.getAllJobs().subscribe(
+      res=>{
+        this.jobs = res;
+        console.log(this.jobs);
+      }
+    )
+  }
+
 }
