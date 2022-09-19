6164f13a9f3d33673f9a1d3977c28725fbd28963
Mirzac Mihai <mihaimarzac@gmail.com>
2022-09-19 17:30:27 +0300

    Added subjects

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,16 +1,24 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, Subject, Subscription } from 'rxjs';
 import { JobModel } from '../models/job.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class JobService {
+export class JobService{
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+  }
+
+  jobSub?: Subscription;
+
+  languageSelected = new Subject<any>();
+  languageDeleted = new Subject<any>();
 
   getAllJobs(): Observable<JobModel[]>{
     return this.http.get<JobModel[]>("assets/data.json");
   }
+
+  
 }
