6f5bd0f846f94e3a67d776b434ed5633df9ae703
Mirzac Mihai <mihaimarzac@gmail.com>
2022-09-19 17:36:38 +0300

    Added components and styled the interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,38 @@
 import { Component } from '@angular/core';
 import { JobModel } from './models/job.model';
-import { OnInit } from '@angular/core';
+import { OnInit, OnDestroy } from '@angular/core';
 import { JobService } from './services/job.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
 
   jobs: JobModel[] = [];
+  languages: string[] = [];
+  jobSub!: Subscription;
 
   constructor(private jobService: JobService){}
 
 
   ngOnInit(): void{
-    this.jobService.getAllJobs().subscribe(
-      res=>{
-        this.jobs = res;
+ 
+    this.jobSub = this.jobService.getAllJobs().subscribe(
+      (resp: any)=>{
+        this.jobs = resp;
+        this.languages = resp.languages;
         console.log(this.jobs);
       }
     )
   }
+
+  ngOnDestroy(): void {
+    this.jobSub.unsubscribe();
+  }
+
+ 
 
 }
